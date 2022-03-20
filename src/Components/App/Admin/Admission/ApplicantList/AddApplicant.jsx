9f9e2ae6156cf0be407266e94d3e9b8953fab01e
midlajc <mail.midlajc@gmail.com>
2022-03-20 07:41:57 +0530

    add apllicant panel ++

--- src/Components/App/Admin/Admission/ApplicantList/AddApplicant.jsx
+++ src/Components/App/Admin/Admission/ApplicantList/AddApplicant.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import CloseIcon from '@mui/icons-material/Close';
+import { Button } from '@mui/material';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const style = {
     position: 'absolute',
-    top:'5%',
-    width: '80%',
+    top: '5%',
+    width: '90%',
     bgcolor: 'background.paper',
     boxShadow: 10,
     p: 4,
@@ -18,7 +24,7 @@ function AddApplicant({ open, close }) {
             <Modal
                 style={{
                     display: 'flex',
-                    justifyContent:'center',
+                    justifyContent: 'center',
                     overflow: "scroll"
                 }}
                 open={open}
@@ -27,14 +33,65 @@ function AddApplicant({ open, close }) {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Text in a modal
+                    <Button sx={{ color: 'black', float: 'right' }} onClick={close}>
+                        <CloseIcon sx={{ width: 15 }} />
+                    </Button>
+                    <Typography sx={{
+                        marginBottom:3
+                    }} id="modal-modal-title" variant="h5" component="h2">
+                        ADD APPLICANT
                     </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    {/* <hr /> */}
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography>Accordion 1</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                        >
+                            <Typography>Accordion 2</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel3a-content"
+                            id="panel3a-header"
+                        >
+                            <Typography>Disabled Accordion</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    {/* <Typography id="modal-modal-description" sx={{ mt: 2 }}>
                         Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
                     </Typography><Typography id="modal-modal-title" variant="h6" component="h2">
                         Text in a modal
-                    </Typography>
+                    </Typography> */}
                 </Box>
             </Modal>
         </div>
