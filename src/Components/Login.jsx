3a430d1804a194229baceee98a88866445022d2a
midlajc <mail.midlajc@gmail.com>
2022-04-16 11:40:02 +0530

    login page loading added

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,8 @@
 import './Login.css';
 import React from 'react'
-import { useState} from 'react';
-import { Grid, Paper, Avatar, TextField, Button, Alert } from '@mui/material'
+import { useState } from 'react';
+import { Grid, Paper, Avatar, TextField, Alert } from '@mui/material'
+import LoadingButton from '@mui/lab/LoadingButton';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import authService from '../service/authService';
 import { useNavigate } from "react-router-dom";
@@ -15,21 +16,25 @@ function Login() {
   const [dAlert, setDAlert] = useState(null);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = React.useState(false);
 
   const doLogin = () => {
+    setLoading(true)
+    setDAlert(null)
     authService.login(username, password).then(res => {
       navigate('/')
       window.location.reload()
     }).catch(err => {
+      setLoading(false)
       setDAlert(err.data.message)
     })
   }
 
   const paperStyle = { padding: 20, height: 'auto', width: 310, margin: "0px 0", paddingBottom: '2rem' }
   const avatarStyle = { backgroundColor: '#1bbd7e' }
-  const btnStyle = { margin: '8px 0' }
+  const btnStyle = { margin: '7px 0' }
   return (
-    <div className='loginGrid' style={{backgroundImage:`url('${background}')`}}>
+    <div className='loginGrid' style={{ backgroundImage: `url('${background}')` }}>
       <Grid className=''>
         <Paper elevation={10} style={paperStyle}>
           <Grid align='center'>
@@ -49,9 +54,15 @@ function Login() {
           <TextField label='Password'
             style={{ marginBottom: '1rem' }} placeholder='Enter password'
             onChange={(e) => setPassword(e.target.value)} type='password' fullWidth required />
-          <Button onClick={doLogin}
+          <LoadingButton
+            onClick={doLogin}
+            loading={loading}
             color='primary' variant="contained"
-            style={btnStyle} fullWidth>Log in</Button>
+            style={btnStyle} fullWidth>Log in
+          </LoadingButton>
+          {/* <Button onClick={doLogin}
+            color='primary' variant="contained"
+            style={btnStyle} fullWidth>Log in</Button> */}
         </Paper>
       </Grid>
     </div>
