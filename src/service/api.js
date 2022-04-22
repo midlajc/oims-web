c11c5d824c314f286d744c1ee1c39d4ca77e70d9
midlajc <mail.midlajc@gmail.com>
2022-04-22 11:05:57 +0530

    .env files added

--- src/service/api.js
+++ src/service/api.js
@@ -1,56 +1,56 @@
 import axios from "axios";
 import storageService from "./storageService";
 const instance = axios.create({
-    // baseURL: "http://192.168.183.13:8000",
-    // baseURL: "http://localhost:8000",
-    baseURL: "https://pro-back-end.herokuapp.com/",
-    // baseURL: "https://tezla.tech/",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: process.env.REACT_APP_API_ENDPOINT,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 instance.interceptors.request.use(
-    (config) => {
-        const token = storageService.getLocalAccessToken();
-        const username = storageService.getUserName();
-        if (token) {
-            config.headers["Authorization"] = 'Bearer ' + token; 
-            config.headers["username"] = username;
-            // config.headers["x-access-token"] = token; 
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = storageService.getLocalAccessToken();
+    const username = storageService.getUserName();
+    if (token) {
+      config.headers["Authorization"] = "Bearer " + token;
+      config.headers["username"] = username;
+      // config.headers["x-access-token"] = token;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 instance.interceptors.response.use(
-    (res) => {
-        return res;
-    },
-    async (err) => {
-        let originalConfig = err.config;
-        if (originalConfig === undefined)
-            originalConfig={url : ''}
-        if (originalConfig.url !== "/auth/login" && err.response) {
-            // Access Token was expired
-            if (err.response.status === 403 && !originalConfig._retry) {
-                originalConfig._retry = true;
-                try {
-                    const rs = await instance.post("/auth/token", {
-                        refreshToken: storageService.getLocalRefreshToken(),
-                    }, {
-                        username: storageService.getUserName()
-                    });
-                    const { accessToken } = rs.data;
-                    storageService.updateLocalAccessToken(accessToken);
-                    return instance(originalConfig);
-                } catch (_error) {
-                    return Promise.reject(_error);
-                }
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    let originalConfig = err.config;
+    if (originalConfig === undefined) originalConfig = { url: "" };
+    if (originalConfig.url !== "/auth/login" && err.response) {
+      // Access Token was expired
+      if (err.response.status === 403 && !originalConfig._retry) {
+        originalConfig._retry = true;
+        try {
+          const rs = await instance.post(
+            "/auth/token",
+            {
+              refreshToken: storageService.getLocalRefreshToken(),
+            },
+            {
+              username: storageService.getUserName(),
             }
+          );
+          const { accessToken } = rs.data;
+          storageService.updateLocalAccessToken(accessToken);
+          return instance(originalConfig);
+        } catch (_error) {
+          return Promise.reject(_error);
         }
-        return Promise.reject(err);
+      }
     }
+    return Promise.reject(err);
+  }
 );
 export default instance;
