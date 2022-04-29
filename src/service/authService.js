7df090b446b1b4920fb4e2320fc3df02f52150d3
midlajc <mail.midlajc@gmail.com>
2022-04-29 08:38:27 +0530

    force at user level

--- src/service/authService.js
+++ src/service/authService.js
@@ -1,65 +1,80 @@
-import api from './api'
-import storageService from './storageService';
+import api from "./api";
+import storageService from "./storageService";
 
 const signup = (email, password) => {
-    return api.post("/signup", {
-        email,
-        password,
-    }).then((response) => {
-        if (response.data.accessToken) {
-            storageService.setUser(response.data)
-        }
-        return response.data;
+  return api
+    .post("/signup", {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        storageService.setUser(response.data);
+      }
+      return response.data;
     });
 };
 
 const login = (username, password) => {
-    return new Promise((resolve, reject) => {
-        api.post("/auth/login", {
-            username,
-            password,
-        }, {
-            headers: {
-                username: username
-            }
-        }).then((response) => {
-            if (response.data.accessToken) {
-                localStorage.setItem("user", JSON.stringify(response.data));
-            }
-            resolve(response.data);
-        }).catch(err => {
-            console.log(err);
-            reject(err.response)
-        });
-    })
+  return new Promise((resolve, reject) => {
+    api
+      .post(
+        "/auth/login",
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            username: username,
+          },
+        }
+      )
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        resolve(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err.response);
+      });
+  });
 };
 
 const logout = () => {
-    let user = JSON.parse(localStorage.getItem('user'))
-    api.post("/auth/logout", {
+  let user = JSON.parse(localStorage.getItem("user"));
+  api
+    .post(
+      "/auth/logout",
+      {
         username: user.username,
-        refreshToken: user.refreshToken
-    }, {
+        refreshToken: user.refreshToken,
+      },
+      {
         headers: {
-            username: user.username
-        }
-    }).then(() => {
-        storageService.removeUser()
-        window.location.reload()
-    }).catch(err => {
-        console.log(err)
+          username: user.username,
+        },
+      }
+    )
+    .then(() => {})
+    .catch((err) => {
+      console.log(err);
     });
+  storageService.removeUser();
+  window.location.reload();
 };
 
 const getCurrentUser = () => {
-    return storageService.getUser()
+  return storageService.getUser();
 };
 
 const authService = {
-    signup,
-    login,
-    logout,
-    getCurrentUser,
+  signup,
+  login,
+  logout,
+  getCurrentUser,
 };
 
 export default authService;
