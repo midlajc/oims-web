4ccb0dc5f0993ff793db9c003f35debd1f447c29
midlajc <mail.midlajc@gmail.com>
2022-05-08 21:38:01 +0530

    404 page back to home added

--- src/Components/404.jsx
+++ src/Components/404.jsx
@@ -1,15 +1,31 @@
+import { Button, Typography } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function NotFound() {
+    const navigate = useNavigate()
     return (
         <div
-        style={{
-            display:'flex',
-            justifyContent:'center',
-            width:'100%',
-            height:'80vh',
-            alignItems:'center'
-        }}>404 Not Found</div>
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                width: '100%',
+                height: '80vh',
+                alignItems: 'center'
+            }}>
+            <Typography variant="overline" display="block">
+                404 Not Found
+            </Typography>
+            <Button variant="outlined"
+                size='small'
+                onClick={() => {
+                    navigate('/')
+                }}
+                display="block">
+                Back to Home
+            </Button>
+        </div>
     )
 }
 
